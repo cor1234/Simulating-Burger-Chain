e0fd17c405dc34f91314b17ac2bc4c799d68cfee
Rohan Kumar <rohandbg2019.rk@gmail.com>
2023-08-16 22:47:04 +0530

    burger

---
+++ Growable_Array.java
@@ -0,0 +1,22 @@
+public class Growable_Array {
+    int top = 0;
+    QueueNode array[] = new QueueNode[2];
+
+    public void push(QueueNode queue_node){
+        if(size() == array.length){
+            QueueNode new_array[] = new QueueNode[2*size()-1];
+            int i = 1;
+            while(i < size()){
+                new_array[i] = array[i];
+                i++;
+            }
+            array = new_array;
+        }
+        array[top + 1] = queue_node;
+        top++;
+    }
+
+    public int size(){
+        return top + 1;
+    }
+}
